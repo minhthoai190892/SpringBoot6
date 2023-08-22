22a2bde63f454c7d249f45f0c99a6a2f671e4cea
ASUS <minhthoai190892@gmail.com>
2023-08-22 16:58:41 +0700

    main

---
+++ eazybytes/src/main/java/com/eazybytes/school/EazybytesApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazybytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazybytesApplication.class, args);
+	}
+
+}
---
+++ eazybytes/src/test/java/com/eazybytes/school/EazybytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazybytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
