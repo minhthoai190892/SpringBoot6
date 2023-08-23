be94fcf7b365c7e6da6139b7f22080aeb031a94a
ASUS <minhthoai190892@gmail.com>
2023-08-23 16:46:48 +0700

    100.-Demo-of-Bean-Validations-inside-Contact-form-Page

--- eazybytes/src/main/java/com/eazybytes/school/controller/ContactController.java
+++ eazybytes/src/main/java/com/eazybytes/school/controller/ContactController.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
 import com.eazybytes.school.model.Contact;
 import com.eazybytes.school.service.ContactService;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class ContactController {
 	private static Logger log = LoggerFactory.getLogger(ContactController.class);
 	@Autowired
 	private ContactService contactService;
 	@RequestMapping("/contact")
-	public String displayContactPage() {
+	public String displayContactPage(Model model) {
+		model.addAttribute("contact",new Contact());
 		return "contact.html";
 	}
 //	@RequestMapping(value = "/saveMsg",method = RequestMethod.POST)
@@ -32,9 +38,12 @@ public class ContactController {
 //		
 //	}
 	@RequestMapping(value = "/saveMsg",method = RequestMethod.POST)
-	public ModelAndView saveMessage(Contact contact) {
-		
+	public String saveMessage(@Valid @ModelAttribute("contact")Contact contact,Errors errors) {
+		if (errors.hasErrors()) {
+			log.error("Contact form validation failed due to: "+errors.toString());
+			return "contact.html";
+		}
 		contactService.saveMessageDetails(contact);
-		return new ModelAndView("redirect:/contact");
+		return ("redirect:/contact");
 	}
 }
