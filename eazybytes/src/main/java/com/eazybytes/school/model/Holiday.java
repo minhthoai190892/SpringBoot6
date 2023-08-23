196e2c2778cad3238c9391cf25365fc8e9b3d926
ASUS <minhthoai190892@gmail.com>
2023-08-23 13:28:17 +0700

    88.-Building-Holidays-Page-of-Eazy-School-Web-Application

---
+++ eazybytes/src/main/java/com/eazybytes/school/model/Holiday.java
@@ -0,0 +1,32 @@
+package com.eazybytes.school.model;
+
+public class Holiday {
+	private final String day;
+	private final String reason;
+	private final Type type;
+	public enum Type {
+		FESTIVAL,FEDERAL
+	}
+	public Holiday(String day, String reason, Type type) {
+		super();
+		this.day = day;
+		this.reason = reason;
+		this.type = type;
+	}
+	public String getDay() {
+		return day;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public Type getType() {
+		return type;
+	}
+	@Override
+	public String toString() {
+		return "Holiday [day=" + day + ", reason=" + reason + ", type=" + type + "]";
+	}
+
+	
+	
+}
