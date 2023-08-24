e6b85ec3db14a900c084967b6e83af9e7786e74e
ASUS <minhthoai190892@gmail.com>
2023-08-24 08:53:25 +0700

    103.-Demo-of-@RequestScope-inside-Eazy-School-Web-Application

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/77/f07708c51f42001e161b86ff58847c1a
@@ -0,0 +1,24 @@
+package com.eazybytes.school.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.eazybytes.school.controller.ContactController;
+import com.eazybytes.school.model.Contact;
+
+@Service
+public class ContactService {
+	private static Logger log = LoggerFactory.getLogger(ContactController.class);
+	/**
+	 * Save contact details into DB
+	 * @param contact
+	 * @return boolean
+	 * */
+	public boolean saveMessageDetails(Contact contact) {
+		boolean isSave = true;
+		log.info(contact.toString());
+		return isSave;
+		
+	}
+}
--- eazybytes/src/main/java/com/eazybytes/school/service/ContactService.java
+++ eazybytes/src/main/java/com/eazybytes/school/service/ContactService.java
@@ -3,12 +3,25 @@ package com.eazybytes.school.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 import com.eazybytes.school.controller.ContactController;
 import com.eazybytes.school.model.Contact;
 
 @Service
+@RequestScope
 public class ContactService {
+	private int counter=0;
+	
+	public ContactService() {
+		System.err.println("Contact Service Bean initialized");
+	}
+	public int getCounter() {
+		return counter;
+	}
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
 	private static Logger log = LoggerFactory.getLogger(ContactController.class);
 	/**
 	 * Save contact details into DB
@@ -16,6 +29,7 @@ public class ContactService {
 	 * @return boolean
 	 * */
 	public boolean saveMessageDetails(Contact contact) {
+		System.out.println("saveMessageDetails");
 		boolean isSave = true;
 		log.info(contact.toString());
 		return isSave;
