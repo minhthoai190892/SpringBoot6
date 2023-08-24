91ff328497477a727a2c034faf273beb946b8fa5
ASUS <minhthoai190892@gmail.com>
2023-08-24 14:15:49 +0700

    116.-Configure-multiple-users-using-inMemoryAuthentication-of-Spring-Security

--- eazybytes/src/main/java/com/eazybytes/school/config/ProjectSecurityConfig.java
+++ eazybytes/src/main/java/com/eazybytes/school/config/ProjectSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.eazybytes.school.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		http.csrf(csrf->csrf.disable()) vô hiệu hóa csrf khi không dùng th:acction của thymeleaf
 		http.authorizeHttpRequests((requests)->requests
 //												.requestMatchers("/contact").authenticated()//yêu cầu quền đăng nhập
 //												.requestMatchers("/coursess").denyAll()//ngừng cung cấp dịch vụ cho trang web
@@ -28,5 +35,14 @@ public class ProjectSecurityConfig {
 		;
 		return http.build();
 		
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		UserDetails user = users.username("user1").password("user1").roles("USER").build();
+		UserDetails user2 = users.username("user2").password("user2").roles("USER").build();
+		UserDetails admin = users.username("admin1").password("admin1").roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user,user2,admin);
 	}
 }
